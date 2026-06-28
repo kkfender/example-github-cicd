1ddb4e07f87e0f5045b3858a53724a6ddd569be8
kkfender <ibanez19851030@yahoo.co.jp>
2026-06-28 15:01:33 +0900

    Update TestEvenOrOdd to check for odd number input

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestEvenOrOdd(t *testing.T) {
-	tests := EvenOrOdd(10)
+	tests := EvenOrOdd(11)
 	if tests != "even" {
 		t.Errorf("expected even, actual %s", tests)
 	}
